9bb3e5b6e9ded72d72ee6e0149ce85dea807094f
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2018-12-07 13:38:41 +0200

    fix browser js code to  node

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,29 @@
+const {api}  = require('./api')
+
+App = App || {}
+App.showPostsForCurrentUser = (userId, cb) => {
+  App.getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`
+    })
+    cb(postTemplates)
+  })
+}
+
+App.showUserProfile = (userId, cb) => {
+  App.getUserById(userId, user => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `
+    cb(user)
+  })
+}
+
+
